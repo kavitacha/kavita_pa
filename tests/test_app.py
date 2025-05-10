046580db1d34c6ad71178b1da3dd9842a683b0e0
kavitacha <kavita.chandawar@fractal.ai>
2025-05-10 16:50:22 +0530

    Add basic unit test

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_predict_response():
+    payload = {
+        "Warehouse_block": 1,
+        "Mode_of_Shipment": 0,
+        "Customer_care_calls": 4,
+        "Customer_rating": 3,
+        "Cost_of_the_Product": 200.0,
+        "Prior_purchases": 2,
+        "Product_importance": 1,
+        "Gender": 1,
+        "Discount_offered": 10.0,
+        "Weight_in_gms": 2000.0
+    }
+    response = client.post("/predict", json=payload)
+    assert response.status_code == 200
+    assert response.json()["prediction"] in ["On-Time", "Delayed"]
